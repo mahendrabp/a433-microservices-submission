eaf5fbfd885961ce8fdb84823c7631733f9e8dff
mahendrabp <chipxitro@gmail.com>
2023-08-22 22:52:58 +0700

    feat: add github action and build image for ui branch

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# variable
+IMAGE_NAME="karsajobs-ui"
+IMAGE_TAG="latest"
+
+
+# build docker image
+sudo docker build -t "$IMAGE_NAME:$IMAGE_TAG" .
+
+
+# tag docker image
+sudo docker tag "$IMAGE_NAME:$IMAGE_TAG" "$DOCKERHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG"
+
+# you can skip the login if you're already logged in
+echo "$DOCKERHUB_PASSWD" | sudo docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+
+
+# push the image
+sudo docker push "$DOCKERHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG"
